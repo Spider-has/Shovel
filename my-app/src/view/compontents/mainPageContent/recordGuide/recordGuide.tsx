12b558356e74b0c119c832379037571c969ad0af
Spider-has <romanh998@gmail.com>
2023-05-05 22:52:08 +0300

    Guide

--- my-app/src/view/compontents/mainPageContent/recordGuide/recordGuide.tsx
+++ my-app/src/view/compontents/mainPageContent/recordGuide/recordGuide.tsx
@@ -3,6 +3,7 @@ import "../../../../fonts/fonts.css"
 import stage1Img from "../../../../images/pageImages/mainPage/recordGuideImg1.jpg"
 import stage2Img from "../../../../images/pageImages/mainPage/recordGuideImg2.jpg"
 import stage3Img from "../../../../images/pageImages/mainPage/recordGuideImg3.jpg"
+import {ReactNode} from "react";
 
 
 const Header = () => {
@@ -29,10 +30,80 @@ const Header = () => {
     )
 }
 
-const RecordGuideArea = () => {
+interface guidesStage{
+    index: number,
+    header: string,
+    content: string,
+    image: string,
+}
+interface stages{
+    guides: Array<guidesStage>
+}
+
+
+const RecordGuides = (props: stages) => {
+    let guideStages: Array<ReactNode>;
+    guideStages = props.guides.map(({index, header, content, image}) => {
+        return(
+            <article className={"guide"}>
+                <div className={"guide__guide-text-wrapper"}>
+                    <div className={"guide-header-index"}>
+                        <span className = {"record-nums-text"}>
+                            {index}
+                        </span>
+                    </div>
+                    <div className ={"guide-content-area"}>
+                    <div>
+                        <h3 className={"guide-heading usual-header-text-sm"}>
+                            {header}
+                        </h3>
+                    </div>
+                    <div>
+                        <span className={"usual-content-text"}>
+                            {content}
+                        </span>
+                    </div>
+                    </div>
+                </div>
+                <div className = {"guide__image"}>
+                    <img src = {image} alt={"guide-stage " + index}/>
+                </div>
+            </article>
+        )
+    })
     return(
-        <section className={"map"}>
+        <div className={"guides-box"}>
+            {guideStages}
+        </div>
+    )
+}
+
+const RecordGuideArea = () => {
+    let guide: stages = {
+       guides: [{
+           index: 1,
+           header: "Создайте аккаунт",
+           content: "Чтобы участвовать в экспедиции достаточно зарегистрироваться на площадке добро.ру",
+           image: stage1Img,
+       },
+       {
+           index: 2,
+           header: "Оформите свой профиль",
+           content: "Возраст, увлечения, контактную информацию и другое. Это поможет нам узнать вас перед приглашением в экспедицию",
+           image: stage2Img,
+       },
+       {
+           index: 3,
+           header: "Отправляйте заявку",
+           content: "Посмотрите дату экспедиции, узнайте свои обязанности и оставляйте заявку, мы посмотрим ваш профиль и свяжемся с вами",
+           image: stage3Img,
+       },
+       ]
+    };
+    return(
+        <section className={"guides-area"}>
             <Header/>
+            <RecordGuides guides={guide.guides}/>
         </section>
     )
 }
